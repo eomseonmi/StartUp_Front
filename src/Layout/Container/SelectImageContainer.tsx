de313966730367fc2d9cff11b08a8a3dfca67335
eom seonmi <vsmumv@gmail.com>
2023-06-29 16:08:17 +0900

    이미지 연동 관련 수정

--- src/Layout/Container/SelectImageContainer.tsx
+++ src/Layout/Container/SelectImageContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 import SelectImage from "../Presentational/SelectImage";
 import axios from 'axios';
 
@@ -13,18 +13,20 @@ import axios from 'axios';
 // }
 
 const SelectImageContainer = () => {
- alert('Startup SelectImageContainer');
+  const navigate = useNavigate();
   const [imageUrl, setImageUrl] = useState('');
-  alert('Startup main container load data rse');
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://127.0.0.1:5000/getText?str=후배에게 주는 선물", { withCredentials: true });
+        const response = await axios.get("http://127.0.0.1:5000/getImage?str=후배에게 주는 선물", { withCredentials: true });
         const data = response.data;
         
         console.log(data);
         // 이미지 URL을 받아와서 상태 업데이트
         setImageUrl(data);
+
+        // 이미지 URL을 가져왔을 때 페이지 이동
+        navigate("/selectImage");
         
       } catch (error) {
         console.error("Error:", error);
@@ -32,7 +34,7 @@ const SelectImageContainer = () => {
     };
 
     fetchData();
-  }, []);
+  }, [navigate]);
   
   return <SelectImage imageUrl={imageUrl} />;
 }
