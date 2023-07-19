a92962dc73a5274f71b23c4a7110d4accc380cbc
hris01 <hris01@hris01ui-MacBookPro.local>
2023-07-19 11:26:39 +0900

    이미지 로딩바 추가

--- src/Layout/Container/SelectImageContainer.tsx
+++ src/Layout/Container/SelectImageContainer.tsx
@@ -1,30 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import axios from 'axios';
+import LoadingModalContainer from './LoadingModalContainer';
 
 import SelectImage from "../Presentational/SelectImage";
 
 const SelectImageContainer = () => {
-  const navigate = useNavigate();
   const location = useLocation();
+  const [loading, setLoading] = useState(false);
   const [imageUrls, setImageUrls] = useState<string[]>([]);
   
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const searchParams = new URLSearchParams(location.search);
         const description = searchParams.get('description');
         
         if (description) {
-          // alert(description);
-        //  const response = await axios.get(`http://127.0.0.1:5000/getImage?str=${description}`, { withCredentials: true });
-          const response = await axios.get(`http://127.0.0.1:5000/getImageTest?str=${description}`, { withCredentials: true });
+           const response = await axios.get(`http://127.0.0.1:5000/getImage?str=${description}`, { withCredentials: true });
+          // const response = await axios.get(`http://127.0.0.1:5000/getImageTest?str=${description}`, { withCredentials: true });
           const data = response.data;
           const urls = data.split("##*##*");
           setImageUrls(urls);
         } else {
           alert("No description provided");
         }
+        setLoading(false);
       } catch (error) {
         console.error("Error:", error);
       }
@@ -33,7 +35,9 @@ const SelectImageContainer = () => {
     fetchData();
   }, [location]);
 
-  return <SelectImage imageUrls={imageUrls} />;
+  return (
+    loading ? <LoadingModalContainer /> : <SelectImage imageUrls={imageUrls} />
+  );
 };
 
 export default SelectImageContainer;
