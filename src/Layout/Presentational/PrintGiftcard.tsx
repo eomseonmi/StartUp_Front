216da7d491bd14ffa4fd64a8fd7ee54867f025af
hris01 <hris01@hris01ui-MacBookPro.local>
2023-07-20 14:38:44 +0900

    선미 정렬분 반영

--- src/Layout/Presentational/PrintGiftcard.tsx
+++ src/Layout/Presentational/PrintGiftcard.tsx
@@ -18,63 +18,52 @@ const PrintGiftcard = () => {
         <StartupHeader />
 
         <div className="ui people shape" style={{ display: 'flex',marginTop : '50px', justifyContent: 'center', alignItems: 'center'}}>
-        <div className="sides">
-         <div className="active side">
-            <div className="ui card" style={{width:'600px', height:'700px'}}>
-            <div className="image" style={{height:'60%'}}>
-                <img src={selectedImg} style={{width:'100%', height : '100%', marginLeft : '-0.25px'}}/> 
-            </div> 
-            <div className="content" style={{height:'30%'}}>
-                 <div className="description" style={{fontSize:'22px' , display: 'flex',alignItems: 'center', height: '100%'}}><p>{selectedValue}</p></div>
-            </div>
-            <div className="extra content" style={{height:'10%',fontSize:'20px', fontStyle : 'oblique' ,color:'darkgreen'}}>
-                <span className="right floated"></span> <span> 
-                <i className="gift icon"/>The Hyundai GiftCard</span> 
-
-            </div> 
-            </div>
-        </div> 
-        <div className="side"> 
-            <div className="ui card"> 
-                <div className="image"> 
-                     <img src={selectedImg}/>
-                </div>
-                <div className="content"> 
-                    <a className="header">Stevie Feliciano</a> 
-                    <div className="meta">
-                         <span className="date">Joined in 2014</span>
-                    </div> 
-                    <div className="description">{selectedValue}</div>
-                </div>
-                <div className="extra content">
-                    <a><i className="gift icon" />The Hyundai GiftCard</a>
+            <div className="sides">
+                <div className="active side">
+                    <div className="ui card" style={{width:'600px', height:'700px'}}>
+                        <div className="image" style={{height:'60%'}}>
+                            <img src={selectedImg} style={{width:'100%', height : '100%', marginLeft : '-0.25px'}}/> 
+                        </div> 
+                        <div className="content" style={{height:'30%'}}>
+                            <div className="description" style={{fontSize:'22px' , display: 'flex',alignItems: 'center', height: '100%'}}><p>{selectedValue}</p>
+                            </div>
+                        </div>
+                        <div className="extra content" style={{height:'10%',fontSize:'20px', fontStyle : 'oblique' ,color:'darkgreen'}}>
+                            <span className="right floated"></span> <span> 
+                            <i className="gift icon"/>The Hyundai GiftCard</span> 
+                        </div> 
+                    </div>
                 </div> 
-            </div> 
-        </div>
-        </div>
+                <div className="side"> 
+                    <div className="ui card"> 
+                        <div className="image"> 
+                            <img src={selectedImg}/>
+                        </div>
+                        <div className="content"> 
+                            <a className="header">Stevie Feliciano</a> 
+                            <div className="meta">
+                                <span className="date">Joined in 2014</span>
+                            </div> 
+                            <div className="description">{selectedValue}
+                            </div>
+                        </div>
+                        <div className="extra content">
+                            <a><i className="gift icon" />The Hyundai GiftCard</a>
+                        </div> 
+                    </div> 
+                </div>
+            </div>
         </div>
 
         <div className="printbtn">
-
                 <div>
-
-                    <button className="huge ui right labeled icon button" >
-
+                    <button className="huge ui right labeled icon button printbtn" >
                         <i className="right arrow icon"></i>
-
                         출력하기
-
                     </button>
-
                 </div>
-
         </div>
-
-
-
       </>
-    );
-  
-    };
+    );};
 
 export default PrintGiftcard;
